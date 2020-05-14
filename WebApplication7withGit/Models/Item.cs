bb1fb50503d8fa32650c6f0fc79315e2ad72f206
Khan Shafiqul Islam Murad <moonuiu2005@gmail.com>
2020-05-14 16:53:24 +0600

    Adding price property

--- WebApplication7withGit/Models/Item.cs
+++ WebApplication7withGit/Models/Item.cs
@@ -11,7 +11,9 @@ namespace WebApplication7withGit.Models
         public int ItemNo { get; set; }
         public int ItemId { get; set; }
         public string ItemName { get; set; }
-      
+
+        public decimal ItemPrice { get; set; }
+
         public Origin Origin { get; set; }
         public int OriginId { get; set; }
 
