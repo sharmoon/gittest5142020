a6bd06b81c1f7229b2f70bbdf34e77a513c04e9e
Khan Shafiqul Islam Murad <moonuiu2005@gmail.com>
2020-05-14 16:16:03 +0600

    adding item model

---
+++ WebApplication7withGit/Models/Item.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication7withGit.Models
+{
+    public class Item
+    {
+        public int Id { get; set; }
+    }
+}
